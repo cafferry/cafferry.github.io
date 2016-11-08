25957c86efa7a7f2185cb05266fd8a8588187a09
cafferry <382332789@qq.com>
2016-11-08 19:08:55 +0800

    parkoiur

--- parkour/js/game.js
+++ parkour/js/game.js
@@ -1,5 +1,4 @@
-/*粒子动画*/
-function fire(cobj) {
+function lizi(cobj) {
     this.cobj = cobj
     this.x = 0;
     this.y = 0;
@@ -13,7 +12,7 @@ function fire(cobj) {
     this.r = 1;
     this.color = "#fef";
 }
-fire.prototype = {
+lizi.prototype = {
     draw: function () {
         var cobj = this.cobj;
         cobj.save();
@@ -22,9 +21,7 @@ fire.prototype = {
         cobj.translate(this.x, this.y);
         cobj.scale(this.r, this.r)
         cobj.moveTo(0, 0);
-        //cobj.bezierCurveTo(this.x1,this.y1,this.x2,this.y2,0,0);
-        cobj.lineTo(this.x1, this.y1);
-        cobj.lineTo(this.x2, this.y2);
+        cobj.bezierCurveTo(this.x1,this.y1,this.x2,this.y2,0,0);
         cobj.fill();
         cobj.restore();
     },
@@ -36,12 +33,11 @@ fire.prototype = {
     }
 }
 
-
 function stone(cobj, x, y,color) {
-    var color=color||"#fff";
+    var color=color||"#ccc";
     var stoneArr = [];
     for (var i = 0; i < 5; i++) {
-        var obj = new fire(cobj);
+        var obj = new lizi(cobj);
         obj.x = x;
         obj.y = y;
         obj.color=color;
@@ -63,174 +59,174 @@ function stone(cobj, x, y,color) {
 }
 
 
-/*人物*/
-function person(canvas, cobj, runImg, jumpImg) {
-    this.x = canvas.width / 3;
-    this.y = 732;
-    this.endy = 732;
-    this.width = 68;
-    this.height = 68;
-    this.canvas = canvas;
-    this.cobj = cobj;
-    this.runImg = runImg;
-    this.jumpImg = jumpImg;
-    this.status = "runImg";
-    this.state = 0;
-    this.speedy = 5;
-    this.zhongli = 10;
+function person(canvas,cobj,runsImg,jumpImg){
+	this.canvas=canvas;
+	this.canvasH=this.canvas.height;
+	this.cobj=cobj;
+	this.runsImg=runsImg;
+	this.jumpImg=jumpImg;
+	this.x=0;
+	this.y=0;
+	this.width=80;
+	this.height=120;
+	this.endy=this.canvasH-this.height;
+	this.action = "runsImg";
+	this.state = 0;
+	this.speedy = 5;
+	this.gravity=3
 }
-person.prototype = {
-    draw: function () {
-        this.cobj.save();
-        this.cobj.translate(this.x, this.y);
-        this.cobj.drawImage(this[this.status][this.state], 0, 0, 68, 68, 0, 0, this.width, this.height);
-        this.cobj.restore();
-    },
-    update: function () {
-        if (this.y > this.endy) {
-            this.y = this.endy
-            stone(this.cobj, this.x + this.width / 2, this.y + this.height)
-        } else if (this.y < this.endy) {
-            this.speedy += this.zhongli;
-            this.y += this.speedy;
+person.prototype={
+	draw:function(){
+		this.cobj.save();
+		this.cobj.translate(this.x,this.y);
+        if(this.action=="jumpImg"){
+            this.cobj.drawImage(this[this.action][0],0,0,800,1200,0,0,this.width,this.height)
+        }else{
+        	this.cobj.drawImage(this[this.action][this.state],0,0,800,1200,0,0,this.width,this.height)
         }
-    }
-
+		this.cobj.restore()		
+	},
+	update:function(){
+		if (this.y>=this.endy) {
+            this.y= this.endy
+            stone(this.cobj,this.x+this.width/2,this.y+this.height)
+        }else if(this.y<this.endy){
+            this.speedy+=this.gravity;
+            this.y+=this.speedy;
+        }
+	}
 }
 
-
-/*创建障碍物对象*/
-
-function hinder(canvas, cobj, hinderImg) {
-    this.canvas = canvas;
-    this.cobj = cobj;
-    this.hinderImg = hinderImg;
-    this.state = 0;
-    this.x = canvas.width;
-    this.y = 762;
-    this.width=56;
-    this.height=40;
+function block(canvas,cobj,blockImg){
+   this.canvas=canvas;
+   this.cobj=cobj;
+   this.blockImg=blockImg;
+   this.width=145;
+   this.height=85;
+   this.x = canvas.width;
+   this.y=  canvas.height-this.height;
+   this.state=0;  
 }
-hinder.prototype={
-    draw:function(){
-        this.cobj.save();
-        this.cobj.translate(this.x,this.y);
-        this.cobj.drawImage(this.hinderImg[this.state],0,0,565,399,0,0,this.width,this.height);
-        this.cobj.restore();
-    }
+block.prototype={
+   draw:function(){
+      this.cobj.save()
+      this.cobj.translate(this.x,this.y)
+      this.cobj.drawImage(this.blockImg[this.state],0,0,1451,848,0,0,this.width,this.height)
+      this.cobj.restore()
+   }
 }
 
-
-/*游戏主程序*/
-
-function game(canvas, cobj, runImg, jumpImg, hinderImg) {
-    this.canvas = canvas;
-    this.canvasW = canvas.width;
-    this.canvasH = canvas.height;
-    this.cobj = cobj;
-    this.hinderImg = hinderImg;
-    this.hinderArr = [];
-    this.person = new person(canvas, cobj, runImg, jumpImg);
-    this.speed = 8;
-    this.score=1;
-    this.life=3;
+function game(canvas,cobj,runsImg,jumpImg,blockImg){
+	this.canvas=canvas;
+	this.canvasW=this.canvas.width;
+	this.canvasH=this.canvas.height;
+	this.cobj=cobj;
+	this.runsImg=runsImg;
+	this.jumpImg=jumpImg;
+	this.blockImg = blockImg;
+	this.blockArr=[];
+	this.speed=5;
+	this.person=new person(canvas,cobj,runsImg,jumpImg)
+	this.score=0;
+	this.life=4;
+  this.guanka=1;
 }
-game.prototype = {
-    play: function () {
-        var that = this;
-        that.key();
-        var num = 0;
-        var back = 0;
-        var num2=0;
-        var step=8000+parseInt(5*Math.random())*1000;
-        setInterval(function () {
-            num++;
-            back -= that.speed;
-            that.cobj.clearRect(0, 0, that.canvasW, that.canvasH);
-            if (that.person.status == "runImg") {
-                that.person.state = (num %3);
-            } else if (that.person.status == "jumpImg") {
-                that.person.state = 0;
-            }
-            that.person.draw();
-            // that.person.update();
-            that.canvas.style.backgroundPositionX = back + "px";
-            if(num2%step==0){
-                num2=0;
+game.prototype={
+	play:function(){
+		var that=this;
+		that.jump();
+		var num=0;
+		var back=0;
+		var second=0;
+		var step=5000+parseInt(5*Math.random())*1000;
+		setInterval(function(){
+			num++;
+			that.person.state=num%7;
+			that.cobj.clearRect(0,0,that.canvasW,that.canvasH);
+			that.person.draw();
+            that.person.update();
+			that.person.x+=that.speed;
+			if(that.person.x>=that.canvasW/3){
+            	that.person.x=that.canvasW/3;
+         	}
+
+			back-=that.speed;
+			that.canvas.style.backgroundPosition=back+"px";    //支持火狐
+			that.canvas.style.backgroundPositionX=back+"px";     //支持谷歌			
+            if(second%step==0){
+                second=0;
                 step=5000+parseInt(5*Math.random())*1000;
-                    var hinderObj=new hinder(that.canvas,that.cobj,that.hinderImg);
-                    hinderObj.state=Math.floor(that.hinderImg.length*Math.random());
-                    that.hinderArr.push(hinderObj);
-                    if(that.hinderArr.length>5){
-                        that.hinderArr.shift();
-                    }
+                var blockobj=new block(that.canvas,that.cobj,that.blockImg);
+                blockobj.state=Math.floor(that.blockImg.length*Math.random());
+                that.blockArr.push(blockobj);
+                if(that.blockArr.length>5){
+                    that.blockArr.shift();
+                }
             }
+            second+=50;
 
-            num2+=50;
-
-
-
-            for(var i=0;i<that.hinderArr.length;i++){
-                that.hinderArr[i].x-=that.speed;
-                that.hinderArr[i].draw();
-                if(hitPix(that.canvas,that.cobj,that.person,that.hinderArr[i])){
-                    if(!that.hinderArr[i].flag1) {
+            for(var i=0;i<that.blockArr.length;i++){
+                var lifes=document.getElementById('life')
+                var guankas=document.getElementById('guanka')
+                var fenshus=document.getElementById('fenshu')
+                that.blockArr[i].x-=that.speed;
+                that.blockArr[i].draw();
+                if(hitPix(that.canvas,that.cobj,that.person,that.blockArr[i])){
+                    if(!that.blockArr[i].flag) {
                         that.life--;
-                        stone(that.cobj, that.person.x + that.person.width / 2, that.person.y + that.person.height / 2,"red")
+                        lifes.innerHTML=that.life;
                     }
-                    that.hinderArr[i].flag1=true;
+                    that.blockArr[i].flag=true;
+                    stone(that.cobj,that.person.x+that.person.width/2,that.person.y+that.person.height/2,"red")
                     if(that.life<0){
                         alert("game over");
                         location.reload();
                     }
-
-                }else if(that.hinderArr[i].x+that.hinderArr[i].width<that.person.x){
-                    if(!that.hinderArr[i].flag&&!that.hinderArr[i].flag1) {
-                        document.title=++that.score;
+                }else if(that.blockArr[i].x+that.blockArr[i].width<that.person.x){
+                    if(!that.blockArr[i].flag&&!that.blockArr[i].flag1){
+                        that.score++;
+                        fenshus.innerHTML=that.score;
                         if(that.score%3==0){
-                            that.speed+=1;
+                            that.speed++;
+                            that.guanka++;
+                            guankas.innerHTML=that.guanka;
                         }
                     }
-                    that.hinderArr[i].flag=true;
+                    that.blockArr[i].flag1=true;
                 }
-
-
-
             }
-
-        }, 80)
-    },
-    key: function () {
-        var that = this;
-        var flag = true;
-        document.onkeydown = function (e) {
+		},60)
+	},
+	jump:function(){
+      var that=this;
+      var flag=true;
+      document.onkeydown=function(e){
+         var e=e||window.event;
+         var code=e.keyCode;
+         if (code==32) {
+         	that.person.action="jumpImg";
+    			var ang=0;
+    			var inty=that.person.y;
+    			var r=100;
+    			that.speed=8;
             if (!flag) {
-                return;
+               return;
+            }else{
+               flag=false;
+               var t=setInterval(function(){
+                  ang+=5;
+                  if(ang>180){
+                     clearInterval(t)
+                     that.person.action="runsImg";
+                     that.person.y=inty;
+                     flag=true;
+                  }else{
+                     var len=Math.sin(ang*Math.PI/180)*r;
+                     that.person.y=inty-len;
+                  }
+               },60)
             }
-            flag = false;
-            var code = e.keyCode;
-            if (code == 32) {
-
-                that.person.status = "jumpImg";
-                var initA = 0;
-                var speedA = 10;
-                var r = 100;
-                var initY = that.person.y;
-                var t = setInterval(function () {
-                    initA += speedA;
-                    if (initA > 180) {
-                        stone(that.cobj, that.person.x + that.person.width / 2, that.person.y + that.person.height)
-                        clearInterval(t);
-                        that.person.y = initY;
-                        that.person.status = "runImg";
-                        flag = true;
-                    } else {
-                        var len = Math.sin(initA * Math.PI / 180) * r;
-                        that.person.y = initY - len;
-                    }
-                }, 50)
-            }
-        }
-    }
+        }	
+      } 	
+	}
 }
-
